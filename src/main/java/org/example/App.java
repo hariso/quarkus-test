2f1f09f3b386e6abea0bb821faa660e65bc71e25
Haris Osmanagic <haris@meroxa.io>
2023-03-23 13:24:56 +0100

    updates

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,28 +1,13 @@
 package org.example;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.util.Arrays;
-
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.annotations.QuarkusMain;
 
 @QuarkusMain
 public class App {
     public static void main(String... args) {
-        int port;
-        try (ServerSocket socket = new ServerSocket(0)) {
-            port = socket.getLocalPort();
-        } catch (IOException e) {
-            System.err.println("Port is not available");
-            System.exit(1);
-            return;
-        }
-
-        String[] newArgs = Arrays.copyOf(args, args.length + 1);
-        newArgs[newArgs.length - 1] = String.format("-Dquarkus.grpc.server.port=%d", port);
-
+        int port = 9000; // default
         System.out.printf("1|1|tcp|localhost:%d|grpc%n", port);
-        Quarkus.run(newArgs);
+        Quarkus.run(args);
     }
 }
